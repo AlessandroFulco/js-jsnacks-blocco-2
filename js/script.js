fdb9a615501b2536a9e9c14d438490dd065a634d
AlessandroFulco <alessandro.fulco99@gmail.com>
2022-05-12 09:25:15 +0200

    refactory

--- js/script.js
+++ js/script.js
@@ -7,14 +7,20 @@ let sommaNumeriUtente = 0;
 
     
 do {
+    debugger;
     // definizione input utente 
     let numeroUtente = parseInt(prompt('inserisci numer: '));
 
-    // inserisco il numero dentro l'array
-    arrayNumeriUtente.push(numeroUtente);
-
-    // somma di ogni numero dell'utente che inserisce
-    sommaNumeriUtente += numeroUtente;
+    
+    if (numeroUtente < 50) {
+        // inserisco il numero dentro l'array
+        arrayNumeriUtente.push(numeroUtente);
+        // somma di ogni numero dell'utente che inserisce
+        sommaNumeriUtente += numeroUtente;
+        
+    } else  {
+        alert(' il numero o la somma dei numeri deve essere inferiore a 50');
+    }
     
     
 } while (sommaNumeriUtente < 50);
